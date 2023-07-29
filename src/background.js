6611415bf82d8b13fb1fa25732c9674a69bf09ab
Aahanaa Sharrma <sharrma24aahanaa@gmail.com>
2023-07-29 11:01:03 +0530

    stable blocks from popup

--- src/background.js
+++ src/background.js
@@ -1,7 +1,15 @@
 // background.js
+
+// Set up the default blocked websites on installation
 chrome.runtime.onInstalled.addListener(() => {
-  // Default blocked websites (Add more as needed)
-  const defaultBlockedWebsites = ["www.instagram.com","www.facebook.com","www.pornhub.com", "www.wikihow.com"];
+  const defaultBlockedWebsites = [
+    "www.instagram.com",
+    "www.facebook.com",
+    "www.pornhub.com",
+    "www.wikihow.com",
+    "alexadevsrm.com",
+    "www.srmkzilla.net"
+  ];
 
   chrome.storage.sync.set({ blockedWebsites: defaultBlockedWebsites }, () => {
     console.log("Default blocked websites set:", defaultBlockedWebsites);
@@ -9,18 +17,46 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 // Block the page content if the website is blocked
-chrome.webNavigation.onDOMContentLoaded.addListener((details) => {
-  const blockedWebsites = [];
-  chrome.storage.sync.get("blockedWebsites", (data) => {
-    if (data.blockedWebsites) {
-      blockedWebsites.push(...data.blockedWebsites);
-    }
-
-    const currentHostname = new URL(details.url).hostname.toLowerCase();
-    if (blockedWebsites.includes(currentHostname)) {
+chrome.webNavigation.onCommitted.addListener((details) => {
+  const currentHostname = new URL(details.url).hostname.toLowerCase();
+  isWebsiteBlocked(currentHostname, (isBlocked) => {
+    if (isBlocked) {
       // Block the page by redirecting it to a blocked page
       const blockedPage = chrome.runtime.getURL("blocked.html");
       chrome.tabs.update(details.tabId, { url: blockedPage });
     }
   });
+});
+
+// Function to check if a website is blocked
+function isWebsiteBlocked(hostname, callback) {
+  chrome.storage.sync.get("blockedWebsites", (data) => {
+    const blockedWebsites = data.blockedWebsites || [];
+    const isBlocked = blockedWebsites.some((blockedSite) => {
+      const regex = new RegExp(`^${blockedSite.replace(/\./g, "\\.")}$`);
+      return regex.test(hostname);
+    });
+    callback(isBlocked);
+  });
+}
+
+// Listen for messages from the browser action popup
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "addBlockedWebsite") {
+    const websiteToAdd = message.website;
+    if (websiteToAdd) {
+      chrome.storage.sync.get("blockedWebsites", (data) => {
+        const blockedWebsites = data.blockedWebsites || [];
+        if (!blockedWebsites.includes(websiteToAdd)) {
+          blockedWebsites.push(websiteToAdd);
+          chrome.storage.sync.set({ blockedWebsites }, () => {
+            sendResponse({ success: true });
+          });
+        } else {
+          sendResponse({ success: false, message: "Website is already blocked." });
+        }
+      });
+    }
+  }
+  return true; // Indicates that the response will be sent asynchronously
 });
