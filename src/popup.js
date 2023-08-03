dbe9c2ae1edeb730944ccfe95e0c4d25daa19bbb
Aahanaa Sharrma <sharrma24aahanaa@gmail.com>
2023-08-03 12:41:12 +0530

    Revert "unblocks blocks with popup"
    This reverts commit 3af441ca0e55ea8a4865bbee349d635d47330217.

--- src/popup.js
+++ src/popup.js
@@ -1,56 +1,24 @@
 // popup.js
 document.getElementById("blockWebsiteForm").addEventListener("submit", (event) => {
   event.preventDefault();
-  const blockWebsiteInput = document.getElementById("blockWebsiteInput");
-  const websiteToBlock = blockWebsiteInput.value.trim();
-  if (websiteToBlock) {
-    chrome.runtime.sendMessage({ action: "addBlockedWebsite", website: websiteToBlock }, (response) => {
+  const websiteInput = document.getElementById("websiteInput");
+  const website = websiteInput.value.trim();
+  if (website) {
+    chrome.runtime.sendMessage({ action: "addBlockedWebsite", website }, (response) => {
       if (response.success) {
-        showBlockMessage(`Website ${websiteToBlock} has been added to the blocked list.`);
+        showMessage(`Website ${website} has been added to the blocked list.`);
       } else {
-        showBlockMessage(response.message);
+        showMessage(response.message);
       }
     });
   }
 });
 
-document.getElementById("unblockWebsiteForm").addEventListener("submit", (event) => {
-  event.preventDefault();
-  const unblockWebsiteInput = document.getElementById("unblockWebsiteInput");
-  const websiteToUnblock = unblockWebsiteInput.value.trim();
-  if (websiteToUnblock) {
-    chrome.runtime.sendMessage({ action: "removeBlockedWebsite", website: websiteToUnblock }, (response) => {
-      if (response.success) {
-        showUnblockMessage(`Website ${websiteToUnblock} has been removed from the blocked list.`);
-      } else {
-        showUnblockMessage(response.message);
-      }
-    });
-  }
-});
-
-function showBlockMessage(message) {
-  const blockMessageDiv = document.getElementById("blockMessage");
-  blockMessageDiv.textContent = message;
+function showMessage(message) {
+  const messageDiv = document.getElementById("message");
+  messageDiv.textContent = message;
   setTimeout(() => {
-    blockMessageDiv.textContent = "";
+    messageDiv.textContent = "";
   }, 3000);
 }
 
-function showUnblockMessage(message) {
-  const unblockMessageDiv = document.getElementById("unblockMessage");
-  unblockMessageDiv.textContent = message;
-  setTimeout(() => {
-    unblockMessageDiv.textContent = "";
-  }, 3000);
-}
-
-// Add a click event listener for the extension icon
-chrome.action.onClicked.addListener(() => {
-  chrome.windows.create({
-    url: "popup.html",
-    type: "popup",
-    width: 400,
-    height: 300,
-  });
-});
